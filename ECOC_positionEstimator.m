a5ab64da5661a1eca1f1f45876621d0d4e474405
jgs3817 <Josh@Joshuas-MacBook-Pro.local>
2021-03-02 19:02:20 +0700

    ECOC implementation

---
+++ ECOC_positionEstimator.m
@@ -0,0 +1,27 @@
+function [x, y] = ECOC_positionEstimator(test_data, modelParameters)
+
+%     past_current_trial.trialId = testData(tr,direc).trialId;
+%     past_current_trial.spikes = testData(tr,direc).spikes(:,1:t); 
+%     past_current_trial.decodedHandPos = decodedHandPos;
+%     past_current_trial.startHandPos = testData(tr,direc).handPos(1:2,1);
+
+    model = modelParameters{1};
+    % tuning_curves = modelParameters{2};
+    mean_trajectory = modelParameters{3};
+
+    n_units = length(test_data.spikes(:,1));
+    time_step = length(test_data.spikes(1,:));
+    
+    x = zeros(1,n_units);
+    
+    for neural_unit = 1:n_units
+        spks = test_data.spikes(neural_unit,:);
+        x(neural_unit) = 1000*sum(spks)/length(spks);
+    end
+    
+    pred = predict(model,x);
+    
+    x = mean_trajectory(pred,1,time_step) + test_data.startHandPos(1);
+    y = mean_trajectory(pred,2,time_step) + test_data.startHandPos(2);
+    
+end
