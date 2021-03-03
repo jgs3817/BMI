475b28534ef52f63c4770b8c7a6d61a7573a6b29
jgs3817 <Josh@Joshuas-MacBook-Pro.local>
2021-03-03 19:25:15 +0700

    New ECOC classifier added (ECOC196)

---
+++ ECOC196_positionEstimatorTraining.m
@@ -0,0 +1,126 @@
+function [modelParameters] = ECOC196_positionEstimatorTraining(training_data)
+  
+    n_trials = length(training_data(:,1));
+    n_units = length(training_data(1,1).spikes(:,1));
+    n_angles = 8;
+
+    % The testing set may contain a longer spike train than the maximum
+    % in the training set, so max_N is set as 1000 based on prior observation 
+    max_N = 1000;
+    
+    % Tuning curves and mean_trajectory
+    mean_trajectory = zeros(n_angles,3,max_N);
+    tuning_curves = zeros(n_units,n_angles);
+
+    for neural_unit = 1:n_units
+        for direction = 1:n_angles
+            rate = [];
+            if(neural_unit==1)
+                x_movement = zeros(length(training_data(:,direction)),max_N);
+                y_movement = zeros(length(training_data(:,direction)),max_N);
+                z_movement = zeros(length(training_data(:,direction)),max_N);
+            end
+
+            for i = 1:length(training_data(:,direction))
+                spks = training_data(i,direction).spikes(neural_unit,:);
+                N = length(spks);
+                temp_rate = 1000*sum(spks)/N;
+                rate = [rate temp_rate];
+
+                if(neural_unit==1)
+                    x = training_data(i,direction).handPos(1,:) - training_data(i,direction).handPos(1,1);
+                    y = training_data(i,direction).handPos(2,:) - training_data(i,direction).handPos(2,1);
+                    z = training_data(i,direction).handPos(3,:) - training_data(i,direction).handPos(3,1);
+
+                    x_movement(i,:) = [x x(end)*ones(1,max_N-length(x))];
+                    y_movement(i,:) = [y y(end)*ones(1,max_N-length(y))];
+                    z_movement(i,:) = [z z(end)*ones(1,max_N-length(z))];
+                end
+
+            end
+            tuning_curves(neural_unit,direction) = mean(rate);
+
+            if(neural_unit==1)
+                mean_trajectory(direction,1,:) = mean(x_movement,1);
+                mean_trajectory(direction,2,:) = mean(y_movement,1);
+                mean_trajectory(direction,3,:) = mean(z_movement,1);
+            end
+
+        end
+        % Normalise
+        tuning_curves(neural_unit,:) = tuning_curves(neural_unit,:)/max(tuning_curves(neural_unit,:));
+    end
+  
+    % Structure data
+    x = zeros(n_trials*n_angles,2*n_units);
+    y = repmat([1:1:n_angles],n_trials,1);
+    y = y(:);
+
+    Y = cell(n_trials*n_angles,3);
+    
+    for neural_unit = 1:n_units
+        for direction = 1:n_angles
+            for i = 1:n_trials
+                spks = training_data(i,direction).spikes(neural_unit,:);
+                spks = tuning_curves(neural_unit,direction) * ...
+                    1000*sum(spks)/length(spks);
+
+                idx = ((direction-1)*length(training_data(:,direction))) + i;
+                x(idx,neural_unit) = spks;
+                
+                spks = training_data(i,direction).spikes(neural_unit,1:320);
+                spks = 1000*sum(spks)/320;
+                x(idx,n_units+neural_unit) = spks;
+
+                Y{idx,1} = training_data(i,direction).handPos(1,:);
+                Y{idx,2} = training_data(i,direction).handPos(2,:);
+                Y{idx,3} = training_data(i,direction).handPos(3,:);
+            end
+        end
+    end
+
+    X = [x y];
+
+    rng(223);
+    k = 10;
+    n = length(X(:,1));
+
+    c = cvpartition(n,'KFold',k);
+
+    min_RMSE = 1/eps;
+    for i = 1:k
+        train = X(training(c,i),:);
+        testing = X(test(c,i),:);
+
+        Y_train = Y(training(c,i),:);
+        Y_testing = Y(test(c,i),:);
+
+        model = fitcecoc(train(:,1:196),train(:,197));
+        pred = predict(model,testing(:,1:196));
+
+        all_RMSE = 0;
+        for j = 1:length(pred)
+            pred_trajectory = squeeze(mean_trajectory(pred(j),:,:));
+            true_trajectory = [Y_testing{j,1};Y_testing{j,2};Y_testing{j,3}];
+            my_N = length(true_trajectory);
+
+            pred_trajectory(1,:) = pred_trajectory(1,:) + true_trajectory(1,1);
+            pred_trajectory(2,:) = pred_trajectory(2,:) + true_trajectory(2,1);
+            pred_trajectory(3,:) = pred_trajectory(3,:) + true_trajectory(3,1);
+
+            RMSE = true_trajectory(1:2,:) - pred_trajectory(1:2,1:my_N);
+            RMSE = mean(mean(RMSE.^2));
+
+            all_RMSE = all_RMSE + (1/length(pred))*RMSE; 
+        end
+        if(all_RMSE < min_RMSE)
+            min_RMSE = all_RMSE;
+            opt_model = model;
+        end
+    end
+
+    modelParameters = cell(1,3);
+    modelParameters{1} = opt_model;
+    modelParameters{2} = tuning_curves;
+    modelParameters{3} = mean_trajectory;
+end
--- ECOC_testFunction.m
+++ ECOC_testFunction.m
@@ -1,74 +1,101 @@
 %% Continuous Position Estimator Test Script
-
 % Edited version of "testFunction_for_students_MTb"
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ECOC with 
+% x: Nx98 (spike rates of 98 neural units)
+% y: Nx1 (one of eight directions)
+% 
+% Output of ECOC is used to obtain mean trajectory in that direction
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all; close all; clc;
 
-start = datestr(now,'HH:MM:SS');
-
 load('monkeydata_training.mat')
 
-% Set random number generator
-rng(2013);
-ix = randperm(length(trial));
+% showPlots = 1 to show the predicted and actual trajectories
+showPlots = 0;
 
-% Select training and testing data (you can choose to split your data in a different way if you wish)
-trainingData = trial(ix(1:50),:);
-testData = trial(ix(51:end),:);
+% Set random seeds
+my_rng = [2013];
+% my_rng = [2013 223 789 123 2021 123 112 234 377 2000];
+starts = cell(2,length(my_rng));
+all_RMSE = zeros(1,length(my_rng));
 
-fprintf('Testing the continuous position estimator...')
+for i = 1:length(my_rng)
+    i
+    starts{1,i} = datestr(now,'HH:MM:SS');
+    % Set random number generator
+    rng(my_rng(i));
 
-meanSqError = 0;
-n_predictions = 0;  
+    ix = randperm(length(trial));
 
-figure
-hold on
-axis square
-grid
+    % Select training and testing data (you can choose to split your data in a different way if you wish)
+    trainingData = trial(ix(1:50),:);
+    testData = trial(ix(51:end),:);
 
-% Train Model
-modelParameters = ECOC_positionEstimatorTraining(trainingData);
+    % fprintf('Testing the continuous position estimator...')
 
-for tr=1:size(testData,1)
-    display(['Decoding block ',num2str(tr),' out of ',num2str(size(testData,1))]);
-    pause(0.001)
-    for direc=randperm(8) 
-        decodedHandPos = [];
+    meanSqError = 0;
+    n_predictions = 0;  
 
-        times=320:20:size(testData(tr,direc).spikes,2);
-        
-        for t=times
-            past_current_trial.trialId = testData(tr,direc).trialId;
-            past_current_trial.spikes = testData(tr,direc).spikes(:,1:t); 
-            past_current_trial.decodedHandPos = decodedHandPos;
-
-            past_current_trial.startHandPos = testData(tr,direc).handPos(1:2,1); 
-            
-            if nargout('ECOC_positionEstimator') == 3
-                [decodedPosX, decodedPosY, newParameters] = ECOC_positionEstimator(past_current_trial, modelParameters);
-                modelParameters = newParameters;
-            elseif nargout('ECOC_positionEstimator') == 2
-                [decodedPosX, decodedPosY] = ECOC_positionEstimator(past_current_trial, modelParameters);
-            end
-            
-            decodedPos = [decodedPosX; decodedPosY];
-            decodedHandPos = [decodedHandPos decodedPos];
-            
-            meanSqError = meanSqError + norm(testData(tr,direc).handPos(1:2,t) - decodedPos)^2;
-            
-        end
-        n_predictions = n_predictions+length(times);
+    if(showPlots==1)
+        figure
         hold on
-        plot(decodedHandPos(1,:),decodedHandPos(2,:), 'r');
-        plot(testData(tr,direc).handPos(1,times),testData(tr,direc).handPos(2,times),'b')
+        axis square
+        grid
     end
+
+    % Train Model
+    modelParameters = ECOC_positionEstimatorTraining(trainingData);
+
+    for tr=1:size(testData,1)
+    %     display(['Decoding block ',num2str(tr),' out of ',num2str(size(testData,1))]);
+    %     pause(0.001)
+        for direc=randperm(8) 
+            decodedHandPos = [];
+
+            times=320:20:size(testData(tr,direc).spikes,2);
+
+            for t=times
+                past_current_trial.trialId = testData(tr,direc).trialId;
+                past_current_trial.spikes = testData(tr,direc).spikes(:,1:t); 
+                past_current_trial.decodedHandPos = decodedHandPos;
+
+                past_current_trial.startHandPos = testData(tr,direc).handPos(1:2,1); 
+
+                if nargout('ECOC_positionEstimator') == 3
+                    [decodedPosX, decodedPosY, newParameters] = ECOC_positionEstimator(past_current_trial, modelParameters);
+                    modelParameters = newParameters;
+                elseif nargout('ECOC_positionEstimator') == 2
+                    [decodedPosX, decodedPosY] = ECOC_positionEstimator(past_current_trial, modelParameters);
+                end
+
+                decodedPos = [decodedPosX; decodedPosY];
+                decodedHandPos = [decodedHandPos decodedPos];
+
+                meanSqError = meanSqError + norm(testData(tr,direc).handPos(1:2,t) - decodedPos)^2;
+
+            end
+            n_predictions = n_predictions+length(times);
+            
+            if(showPlots==1)
+                hold on
+                plot(decodedHandPos(1,:),decodedHandPos(2,:), 'r');
+                plot(testData(tr,direc).handPos(1,times),testData(tr,direc).handPos(2,times),'b')
+            end
+        end
+    end
+
+    if(showPlots==1)
+        legend('Decoded Position', 'Actual Position')
+    end
+
+    all_RMSE(i) = sqrt(meanSqError/n_predictions);
+
+    starts{2,i} = datestr(now,'HH:MM:SS');
+
 end
 
-legend('Decoded Position', 'Actual Position')
-
-RMSE = sqrt(meanSqError/n_predictions)
-
-finish = datestr(now,'HH:MM:SS');
-
-start
-finish
+mean_RMSE = mean(all_RMSE)
+std_RMSE = std(all_RMSE)
