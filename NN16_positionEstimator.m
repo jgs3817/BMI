d88265fb0cd1c93b7b97e9f9dc2c94351cffb3c4
jgs3817 <Josh@Joshuas-MacBook-Pro.local>
2021-03-16 01:42:13 +0700

    Added NN16

---
+++ NN16_positionEstimator.m
@@ -0,0 +1,30 @@
+function [x, y] = NN16_positionEstimator(test_data, modelParameters)
+
+    n_units = length(test_data.spikes(:,1));
+    n_angles = 8;
+    time_step = length(test_data.spikes(1,:));
+    max_N = 1000;
+    
+    spks = test_data.spikes;
+    fill = max_N - length(spks);
+    extra = mod(fill,300);
+    fill = floor(fill/300);
+    padded = [spks,repmat(spks(:,1:300),[1,fill]),spks(:,1:extra)];
+    spikes = reshape(padded,[1,n_units*max_N]);
+
+    rates = [1000*sum(padded,2)/max_N;...
+            1000*sum(padded(:,1:320),2)/320];
+    
+    dir = predict(modelParameters{n_angles+1,1},rates');
+    
+    netx = modelParameters{dir,1};
+    nety = modelParameters{dir,2};
+    predx = netx(spikes');
+    predx = filter(0.01*ones(1,100), 1, predx);
+    predy = nety(spikes');
+    predy = filter(0.01*ones(1,100), 1, predy);
+    
+    x = predx(time_step) + test_data.startHandPos(1);
+    y = predy(time_step) + test_data.startHandPos(2);
+    
+end
---
+++ NN16_positionEstimatorTraining.m
@@ -0,0 +1,61 @@
+function [modelParameters] = NN16_positionEstimatorTraining(training_data)
+  
+    n_trials = length(training_data(:,1));
+    n_units = length(training_data(1,1).spikes(:,1));
+    n_angles = 8;
+    max_N = 1000;
+    
+    modelParameters = cell(n_angles+1,2);
+    
+    % Structure data
+    xPos = zeros(n_angles*n_trials,max_N);
+    yPos = zeros(n_angles*n_trials,max_N);
+    spikes = zeros(n_angles*n_trials,n_units*max_N);
+    rates = zeros(n_angles*n_trials,2*n_units);
+    class = repmat([1:1:n_angles],n_trials,1);
+    class = class(:);
+
+    for direction = 1:n_angles
+        for i = 1:n_trials
+            idx = ((direction-1)*n_trials) + i;
+            xPos(idx,1:length(training_data(i,direction).handPos(1,:))) = training_data(i,direction).handPos(1,:);
+            xPos(idx,1+length(training_data(i,direction).handPos(1,:)):end) = training_data(i,direction).handPos(1,end);
+            yPos(idx,1:length(training_data(i,direction).handPos(2,:))) = training_data(i,direction).handPos(2,:);
+            yPos(idx,1+length(training_data(i,direction).handPos(2,:)):end) = training_data(i,direction).handPos(2,end);
+
+            spks = training_data(i,direction).spikes;
+            fill = max_N - length(spks);
+            extra = mod(fill,300);
+            fill = floor(fill/300);
+            padded = [spks,repmat(spks(:,1:300),[1,fill]),spks(:,1:extra)];
+            spikes(idx,:) = reshape(padded,[1,n_units*max_N]);
+            
+            rate = [1000*sum(padded,2)/max_N;...
+                1000*sum(padded(:,1:320),2)/320];
+            rates(idx,:) = rate;
+        end
+    end
+
+    xPos = xPos - xPos(:,1);
+    yPos = yPos - yPos(:,1);
+
+    rng(223);
+    trainFcn = 'trainscg';
+    hiddenLayers = [100 200 100];
+    net = fitnet(hiddenLayers,trainFcn);
+    net.trainParam.showWindow = false;
+    net.trainParam.time = 1;
+
+    dir = 1:n_trials:size(xPos,1);
+    for i = 1:length(dir)
+        my_xPos = xPos(dir(i):dir(i)+n_trials-1,:);
+        my_yPos = yPos(dir(i):dir(i)+n_trials-1,:);
+        my_spikes = spikes(dir(i):dir(i)+n_trials-1,:);
+
+        modelParameters{i,1} = train(net,my_spikes',my_xPos');    
+        modelParameters{i,2} = train(net,my_spikes',my_yPos');
+    end
+    
+    modelParameters{n_angles+1,1} = fitcecoc(rates,class);
+
+end
